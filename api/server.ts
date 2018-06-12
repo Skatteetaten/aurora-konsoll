cf9fde8fb5074d4115d07e39d7a158dbee9d1695
Espen Strømland <espen.stromland@skatteetaten.no>
2018-06-12 13:59:27 +0200

    added login

--- api/server.ts
+++ api/server.ts
@@ -1,24 +1,33 @@
+/* tslint:disable:no-console */
 import * as ClientOAuth2 from 'client-oauth2';
 import * as express from 'express';
 
 const app = express();
 
-app.get('/api/login', async (req, res) => {
+const CLIENT_ID = process.env.CLIENT_ID || 'aurora-openshift-console-dev';
+const OPENSHIFT_CLUSTER = process.env.OPENSHIFT_CLUSTER || 'utv';
+
+const CLUSTER_URL =
+  process.env.CLUSTER_URL ||
+  `https://${OPENSHIFT_CLUSTER}-master.paas.skead.no:8443`;
+
+const AUTHORIZATION_URI =
+  process.env.AUTHORIZATION_URI || `${CLUSTER_URL}/oauth/authorize`;
+
+const REDIRECT_URI =
+  process.env.ROUTE_URL || 'http://localhost:3000/accept-token';
+
+app.get('/api/start-login', (req, res) => {
   const openshiftToken = new ClientOAuth2({
-    accessTokenUri: 'https://utv-master.paas.skead.no:8443/oauth/token',
-    authorizationUri: 'https://utv-master.paas.skead.no:8443/oauth/authorize',
-    clientId: 'aurora-openshift-console-dev',
-    clientSecret: 'token',
-    redirectUri: 'http://aurora-konsoll-paas-mokey.utv.paas.skead.no/api/login',
-    scopes: []
+    authorizationUri: AUTHORIZATION_URI,
+    clientId: CLIENT_ID,
+    redirectUri: REDIRECT_URI
   });
 
   const uri = openshiftToken.token.getUri();
-  const token = await openshiftToken.token.getToken(uri);
-  return res.send(token);
+  res.redirect(uri);
 });
 
 app.listen(Number(process.env.HTTP_PORT || 9090), () => {
-  /* tslint:disable:no-console */
-  console.log('started on port 9091');
+  console.log('started on port 9090');
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,51 @@
+import * as qs from 'qs';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  RouteComponentProps
+} from 'react-router-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+const AcceptToken = ({ location }: RouteComponentProps<{}>) => {
+  const data = qs.parse(location.hash.substring(1));
+  window.localStorage.setItem('token', data.access_token);
+  return <Redirect push={true} to="/" />;
+};
+
+interface IApplicationState {
+  loggedIn: boolean;
+}
+
+class Application extends React.Component<{}, IApplicationState> {
+  public state = {
+    loggedIn: false
+  };
+
+  public componentDidMount() {
+    const token = window.localStorage.getItem('token');
+    if (!token && window.location.pathname !== '/accept-token') {
+      window.location.replace('/api/start-login');
+    }
+  }
+
+  public render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path="/" component={App} />
+          <Route exact={true} path="/accept-token" component={AcceptToken} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById(
+  'root'
+) as HTMLElement);
 registerServiceWorker();
