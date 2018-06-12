097510420f98b4887760ae58d523a0c57976a780
Espen Strømland <espen.stromland@skatteetaten.no>
2018-06-12 15:46:36 +0200

    env and title

--- api/server.ts
+++ api/server.ts
@@ -3,21 +3,20 @@ import * as ClientOAuth2 from 'client-oauth2';
 import * as express from 'express';
 
 const app = express();
+const { env } = process;
 
-const CLIENT_ID = process.env.CLIENT_ID || 'aurora-openshift-console-dev';
-const OPENSHIFT_CLUSTER = process.env.OPENSHIFT_CLUSTER || 'utv';
+const CLIENT_ID = env.CLIENT_ID || 'aurora-openshift-console-dev';
+const OPENSHIFT_CLUSTER = env.OPENSHIFT_CLUSTER || 'utv';
 
 const CLUSTER_URL =
-  process.env.CLUSTER_URL ||
-  `https://${OPENSHIFT_CLUSTER}-master.paas.skead.no:8443`;
+  env.CLUSTER_URL || `https://${OPENSHIFT_CLUSTER}-master.paas.skead.no:8443`;
 
 const AUTHORIZATION_URI =
-  process.env.AUTHORIZATION_URI || `${CLUSTER_URL}/oauth/authorize`;
+  env.AUTHORIZATION_URI || `${CLUSTER_URL}/oauth/authorize`;
 
-const REDIRECT_HOST = process.env.ROUTE_URL || 'http://localhost:3000';
+const REDIRECT_HOST = env.ROUTE_URL || 'http://localhost:3000';
 
-const REDIRECT_URI =
-  process.env.REDIRECT_URI || `${REDIRECT_HOST}/accept-token`;
+const REDIRECT_URI = env.REDIRECT_URI || `${REDIRECT_HOST}/accept-token`;
 
 app.get('/api/start-login', (req, res) => {
   const openshiftToken = new ClientOAuth2({
@@ -30,6 +29,6 @@ app.get('/api/start-login', (req, res) => {
   res.redirect(uri);
 });
 
-app.listen(Number(process.env.HTTP_PORT || 9090), () => {
+app.listen(Number(env.HTTP_PORT || 9090), () => {
   console.log('started on port 9090');
 });
