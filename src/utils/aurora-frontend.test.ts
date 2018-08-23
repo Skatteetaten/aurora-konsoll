61d9fe8dcf19eb1c3e7316b02a8a46004ffa935c
Jarle Hansen <jarle.hansen@skatteetaten.no>
2018-08-23 08:20:54 +0200

    added assert for array length

--- src/utils/aurora-frontend.test.ts
+++ src/utils/aurora-frontend.test.ts
@@ -5,6 +5,7 @@ describe('aurora-frontend', () => {
   it('removes duplicates and sorts dropdown options from names', () => {
     const dropdownOptions = toDropdownOptions(['name3', 'NAME1', 'name1', 'name2']);
 
+    expect(dropdownOptions).toHaveLength(3);
     expect(dropdownOptions[0]).toEqual({key: 'name1', text: 'name1'});
     expect(dropdownOptions[1]).toEqual({key: 'name2', text: 'name2'});
     expect(dropdownOptions[2]).toEqual({key: 'name3', text: 'name3'});
