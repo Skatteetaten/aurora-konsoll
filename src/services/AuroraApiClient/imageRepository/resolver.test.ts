4429d73b1e087159455c2abc0778e210a281584a
Espen Strømland <espen.stromland@skatteetaten.no>
2018-09-10 15:59:00 +0200

    added test

--- src/services/AuroraApiClient/imageRepository/resolver.test.ts
+++ src/services/AuroraApiClient/imageRepository/resolver.test.ts
@@ -10,23 +10,37 @@ afterAll(() => {
   mockServer.close();
 });
 
-it('should fetch tags from GraphQL server and normalize data', async () => {
-  mockServer.putResponse('getTags', getTags);
+describe('findTagsPaged', () => {
+  it('should fetch tags from GraphQL server and normalize data', async () => {
+    mockServer.putResponse('getTags', getTags);
 
-  const result = await findTagsPaged(mockClient, 'test');
+    const result = await findTagsPaged(mockClient, 'test');
 
-  expect(result).toEqual({
-    endCursor: 'MQ==',
-    hasNextPage: false,
-    tags: [
-      {
-        lastModified: '2018-09-07T07:12:30.345885623Z',
-        name: '2'
-      },
-      {
-        lastModified: '2018-01-25T09:39:51.280498289Z',
-        name: '1'
-      }
-    ]
+    expect(result).toEqual({
+      endCursor: 'MQ==',
+      hasNextPage: false,
+      tags: [
+        {
+          lastModified: '2018-09-07T07:12:30.345885623Z',
+          name: '2'
+        },
+        {
+          lastModified: '2018-01-25T09:39:51.280498289Z',
+          name: '1'
+        }
+      ]
+    });
+  });
+
+  it('should throw an error when no repositories are found', async () => {
+    mockServer.putResponse('getTags', { data: { imageRepositories: [] } });
+
+    try {
+      await findTagsPaged(mockClient, 'mock-repo');
+    } catch (error) {
+      expect((error as Error).message).toEqual(
+        'Could not find tags for repository mock-repo'
+      );
+    }
   });
 });
