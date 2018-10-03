ba5cdbd625615046b129d92833d73b43ff7bfc94
Espen Strømland <espen.stromland@skatteetaten.no>
2018-10-03 08:33:33 +0200

    refactor

--- src/screens/AffiliationViews/DetailsView/InformationView.tsx
+++ src/screens/AffiliationViews/DetailsView/InformationView.tsx
@@ -7,6 +7,7 @@ import {
   IApplicationDeployment,
   IApplicationDeploymentDetails
 } from 'models/ApplicationDeployment';
+import { IDeploymentSpec } from 'models/DeploymentSpec';
 import PodStatus from './PodStatus';
 
 interface IInformationViewProps {
@@ -26,48 +27,16 @@ const InformationView = ({
   if (isFetchingDetails) {
     return <Spinner />;
   }
-  let values: { [key: string]: any } = {};
-  if (deploymentSpec) {
-    const { database, management, certificate } = deploymentSpec;
-    values = {
-      GroupId: deploymentSpec.groupId,
-      // tslint:disable-next-line:object-literal-sort-keys
-      ArtifactId: deploymentSpec.artifactId,
-      Version: deploymentSpec.version
-    };
-    if (database) {
-      values.Database = 'Ja';
-    }
-    if (certificate) {
-      values.Certificate = 'Ja';
-    }
-    if (management) {
-      values.Management = `Path: ${management.path} | Port: ${management.port}`;
-    }
-  }
   return (
     <div className={className}>
       <div className="info-grid">
         <div>
           <h3>Gjeldende AuroraConfig</h3>
-          <InfoContent values={values} />
+          <InfoContent values={getDeploymentSpecValues(deploymentSpec)} />
         </div>
         <div>
           <h3>Aktivt deployment</h3>
-          <InfoContent
-            values={{
-              'Aurora version': deployment.version.auroraVersion,
-              'Image repository': deployment.repository
-                .split('/')
-                .slice(1)
-                .join('/'),
-              Status:
-                deployment.status.code +
-                (deployment.status.comment &&
-                  ` (${deployment.status.comment})`),
-              Tag: deployment.version.deployTag.name
-            }}
-          />
+          <InfoContent values={getApplicationDeploymentValues(deployment)} />
         </div>
       </div>
       <hr
@@ -85,6 +54,42 @@ const InformationView = ({
     </div>
   );
 };
+
+function getDeploymentSpecValues(deploymentSpec?: IDeploymentSpec) {
+  let values: { [key: string]: any } = {};
+  if (deploymentSpec) {
+    const { database, management, certificate } = deploymentSpec;
+    values = {
+      GroupId: deploymentSpec.groupId,
+      ArtifactId: deploymentSpec.artifactId,
+      Version: deploymentSpec.version
+    };
+    if (database) {
+      values.Database = 'Ja';
+    }
+    if (certificate) {
+      values.Certificate = 'Ja';
+    }
+    if (management) {
+      values.Management = `Path: ${management.path} | Port: ${management.port}`;
+    }
+  }
+  return values;
+}
+
+function getApplicationDeploymentValues(deployment: IApplicationDeployment) {
+  return {
+    Tag: deployment.version.deployTag.name,
+    'Aurora version': deployment.version.auroraVersion,
+    'Image repository': deployment.repository
+      .split('/')
+      .slice(1)
+      .join('/'),
+    Status:
+      deployment.status.code +
+      (deployment.status.comment && ` (${deployment.status.comment})`)
+  };
+}
 
 export default styled(InformationView)`
   .labels {
