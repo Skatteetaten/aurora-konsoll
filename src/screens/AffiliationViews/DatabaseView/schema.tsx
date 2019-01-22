4df6096fba75ad9941e1372e640980194e14be2d
Martin Rødland <martin.rodland@skatteetaten.no>
2019-01-22 14:46:54 +0100

    added sorting for dates in database table

--- src/screens/AffiliationViews/DatabaseView/schema.tsx
+++ src/screens/AffiliationViews/DatabaseView/schema.tsx
@@ -11,57 +11,82 @@ interface IColumns {
   fieldName: string;
 }
 
-const columns = [
-  {
-    fieldName: 'type',
-    isResizable: true,
-    key: 0,
-    maxWidth: 100,
-    minWidth: 100,
-    name: 'Type',
-    iconName: 'Down'
-  },
-  {
-    fieldName: 'appDbName',
-    isResizable: true,
-    key: 1,
-    maxWidth: 150,
-    minWidth: 150,
-    name: 'ApplikasjonsDB'
-  },
-  {
-    fieldName: 'createdDate',
-    isResizable: true,
-    key: 2,
-    maxWidth: 100,
-    minWidth: 100,
-    name: 'Opprettet'
-  },
-  {
-    fieldName: 'lastUsedDate',
-    isResizable: true,
-    key: 3,
-    maxWidth: 100,
-    minWidth: 100,
-    name: 'Sist brukt'
-  },
-  {
-    fieldName: 'sizeInMb',
-    isResizable: true,
-    key: 4,
-    maxWidth: 125,
-    minWidth: 125,
-    name: 'Størrelse (MB)'
-  },
-  {
-    fieldName: 'createdBy',
-    isResizable: true,
-    key: 5,
-    maxWidth: 100,
-    minWidth: 100,
-    name: 'Bruker'
+enum SortDirection {
+  ASC,
+  DESC,
+  NONE
+}
+
+const columns = (index: number, sortDirection: SortDirection) => {
+  const cols = [
+    {
+      fieldName: 'type',
+      isResizable: true,
+      key: 0,
+      maxWidth: 100,
+      minWidth: 100,
+      name: 'Type',
+      iconName: ''
+    },
+    {
+      fieldName: 'appDbName',
+      isResizable: true,
+      key: 1,
+      maxWidth: 150,
+      minWidth: 150,
+      name: 'ApplikasjonsDB',
+      iconName: ''
+    },
+    {
+      fieldName: 'createdDate',
+      isResizable: true,
+      key: 2,
+      maxWidth: 100,
+      minWidth: 100,
+      name: 'Opprettet',
+      iconName: ''
+    },
+    {
+      fieldName: 'lastUsedDate',
+      isResizable: true,
+      key: 3,
+      maxWidth: 100,
+      minWidth: 100,
+      name: 'Sist brukt',
+      iconName: ''
+    },
+    {
+      fieldName: 'sizeInMb',
+      isResizable: true,
+      key: 4,
+      maxWidth: 125,
+      minWidth: 125,
+      name: 'Størrelse (MB)',
+      iconName: ''
+    },
+    {
+      fieldName: 'createdBy',
+      isResizable: true,
+      key: 5,
+      maxWidth: 100,
+      minWidth: 100,
+      name: 'Bruker',
+      iconName: ''
+    }
+  ];
+  if (index > -1) {
+    const currentCol = cols[index];
+    if (
+      sortDirection === SortDirection.NONE ||
+      sortDirection === SortDirection.DESC
+    ) {
+      currentCol.iconName = 'Down';
+    } else if (sortDirection === SortDirection.ASC) {
+      currentCol.iconName = 'Up';
+    }
   }
-];
+  return cols;
+};
 
 export interface ISchemaProps {
   onFetch: (affiliations: string[]) => void;
@@ -71,21 +96,21 @@ export interface ISchemaProps {
   className?: string;
 }
 
-enum SortDirection {
-  ASC, DESC, NONE
-}
-
 interface ISchemaState {
   viewItems: IDatabaseSchemaView[];
   columnSortDirections: SortDirection[];
+  selectedColumnIndex: number;
 }
 
-const defaultSortDirections = new Array<SortDirection>(6).fill(SortDirection.NONE);
+const defaultSortDirections = new Array<SortDirection>(6).fill(
+  SortDirection.NONE
+);
 
 export class Schema extends React.Component<ISchemaProps, ISchemaState> {
   public state = {
     viewItems: [],
-    columnSortDirections: defaultSortDirections
+    columnSortDirections: defaultSortDirections,
+    selectedColumnIndex: -1
   };
 
   public sortByColumn = (
@@ -96,34 +121,62 @@ export class Schema extends React.Component<ISchemaProps, ISchemaState> {
     const name = column.fieldName! as keyof any;
 
     const newSortDirections = defaultSortDirections;
-    const sortDirection = columnSortDirections[column.key];
+    const prevSortDirection = columnSortDirections[column.key];
 
-    if(sortDirection === SortDirection.NONE || sortDirection === SortDirection.DESC) {
+    if (
+      prevSortDirection === SortDirection.NONE ||
+      prevSortDirection === SortDirection.DESC
+    ) {
       newSortDirections[column.key] = SortDirection.ASC;
-    } else if(sortDirection === SortDirection.ASC) {
+    } else if (prevSortDirection === SortDirection.ASC) {
       newSortDirections[column.key] = SortDirection.DESC;
     }
-    
+
     const getValue = (value: any) =>
       typeof value === 'string' ? (value as string).toLowerCase() : value;
     const dateValidator = /^\d{1,2}[.]\d{1,2}[.]\d{4}$/;
+
+    const isDate = (value: any) => {
+      return (
+        typeof value === 'string' && (value as string).match(dateValidator)
+      );
+    };
+
+    const createDate = (value: string | null) => {
+      if (value === null) {
+        return new Date(0);
+      } else {
+        const values = value.split('.');
+        return new Date(
+          Number(values[2]),
+          Number(values[1]) - 1,
+          Number(values[0])
+        );
+      }
+    };
+
+    const sortAscending =
+      prevSortDirection === SortDirection.NONE ||
+      prevSortDirection === SortDirection.DESC;
 
     const sortedItems = viewItems.slice(0).sort((a: any, b: any) => {
       const valueA = getValue(a[name]);
       const valueB = getValue(b[name]);
       if (valueA === valueB) {
         return 0;
-      } else if((typeof valueA === 'string') && (valueA as string).match(dateValidator)) {
-        return 0;
+      } else if (isDate(valueA) || isDate(valueB)) {
+        const dateA = createDate(valueA).getTime();
+        const dateB = createDate(valueB).getTime();
+        return sortAscending ? dateB - dateA : dateA - dateB;
       } else {
-        return ((sortDirection === SortDirection.NONE || sortDirection === SortDirection.DESC) 
-          ? valueA < valueB : valueA > valueB) ? 1 : -1;
+        return (sortAscending ? valueA < valueB : valueA > valueB) ? 1 : -1;
       }
     });
 
     this.setState({
       viewItems: sortedItems,
-      columnSortDirections: newSortDirections
+      columnSortDirections: newSortDirections,
+      selectedColumnIndex: column.key
     });
   };
 
@@ -133,9 +186,16 @@ export class Schema extends React.Component<ISchemaProps, ISchemaState> {
 
   public componentDidUpdate(prevProps: ISchemaProps) {
     const { affiliation, items } = this.props;
-    if (prevProps.affiliation !== affiliation || 
-      (prevProps.items.databaseSchemas.length === 0 && items.databaseSchemas.length > 0)) {
+    if (
+      prevProps.affiliation !== affiliation ||
+      (prevProps.items.databaseSchemas.length === 0 &&
+        items.databaseSchemas.length > 0)
+    ) {
       this.handleFetchDatabaseSchemas();
+      this.setState({
+        columnSortDirections: defaultSortDirections,
+        selectedColumnIndex: -1
+      });
     }
   }
   public handleFetchDatabaseSchemas = () => {
@@ -149,7 +209,7 @@ export class Schema extends React.Component<ISchemaProps, ISchemaState> {
           lastUsedDate:
             i.lastUsedDate &&
             new Date(i.lastUsedDate).toLocaleDateString('nb-NO'),
-            appDbName: i.appDbName,
+          appDbName: i.appDbName,
           type: i.type,
           sizeInMb: i.sizeInMb,
           createdBy: i.createdBy
@@ -165,10 +225,10 @@ export class Schema extends React.Component<ISchemaProps, ISchemaState> {
 
   public render() {
     const { isLoading, className } = this.props;
-    const { viewItems } = this.state;
+    const { viewItems, selectedColumnIndex, columnSortDirections } = this.state;
 
-    if(isLoading) {
-      return <Spinner />
+    if (isLoading) {
+      return <Spinner />;
     }
 
     return (
@@ -176,7 +236,10 @@ export class Schema extends React.Component<ISchemaProps, ISchemaState> {
         <div className="styledContainer">
           <div className="styledTable">
             <DetailsList
-              columns={columns}
+              columns={columns(
+                selectedColumnIndex,
+                columnSortDirections[selectedColumnIndex]
+              )}
               items={viewItems}
               onColumnHeaderClick={this.sortByColumn}
             />
@@ -194,6 +257,9 @@ export default styled(Schema)`
   .styledTable {
     grid-area: table;
     margin: 20px auto 0 auto;
+    i {
+      float: right;
+    }
   }
 
   .styledContainer {
