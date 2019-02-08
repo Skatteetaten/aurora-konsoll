601fbf68a003192d92e0105f643e399f1eb9c541
Espen Strømland <espen.stromland@skatteetaten.no>
2019-02-08 14:54:54 +0100

    feature/show-semantic-version

--- src/screens/AffiliationViews/MatrixView/MatrixView.tsx
+++ src/screens/AffiliationViews/MatrixView/MatrixView.tsx
@@ -74,7 +74,7 @@ const MatrixView = ({
         </LoadingButton>
       </StyledUpdate>
     </ActionBar>
-    <Matrix showSemanticVersion={showExactVersion} />
+    <Matrix showSemanticVersion={showSemanticVersion} />
   </div>
 );
 
