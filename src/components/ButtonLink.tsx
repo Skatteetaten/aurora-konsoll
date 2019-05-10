95cc8f88147db1dd496e6d82ca31ae1ed9691853
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-05-10 13:25:44 +0200

    This reverts commit f8c29792609d056a2dfd930009528f49ef15a28d.

---
+++ src/components/ButtonLink.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ButtonLink = styled.button`
+  padding: 0;
+  border: none;
+  font: inherit;
+  color: inherit;
+  background-color: transparent;
+  cursor: pointer;
+  margin: 0 auto;
+`;
