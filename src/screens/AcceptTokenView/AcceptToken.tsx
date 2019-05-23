9f08f29cccbd2cf631961125091b7ce0cab1f7ca
Claus Nielsen <claus.nielsen@skatteetaten.no>
2019-05-23 11:10:16 +0200

    Erstattet bruk av qs lib til URLSearchParams istedet

--- src/screens/AcceptTokenView/AcceptToken.tsx
+++ src/screens/AcceptTokenView/AcceptToken.tsx
@@ -1,4 +1,3 @@
-import * as qs from 'qs';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
@@ -12,17 +11,9 @@ const AcceptToken = ({
   location,
   onTokenUpdated
 }: IAcceptTokenProps & RouteComponentProps<{}>) => {
-  interface IAuthQueryString {
-    expires_in: string;
-    access_token: string;
-  }
-
-  const authQueryString = qs.parse(
-    location.hash.substring(1)
-  ) as IAuthQueryString;
-
-  const token = authQueryString.access_token;
-  const expiresInSeconds = Number(authQueryString.expires_in);
+  const urlParams = new URLSearchParams(window.location.hash.replace("#","?"));  
+  const token = urlParams.get('access_token') as string;
+  const expiresInSeconds = Number(urlParams.get('expires_in'));
   tokenStore.updateToken(token, expiresInSeconds);
 
   onTokenUpdated();
--- src/screens/SecretTokenView/SecretToken.tsx
+++ src/screens/SecretTokenView/SecretToken.tsx
@@ -1,19 +1,10 @@
 import * as React from 'react';
-import * as qs from 'qs';
 
 class SecretTokenNavigation extends React.Component {
 
   public async fetchData() {
-    interface IAuthQueryString {
-      expires_in: string;
-      access_token: string;
-    }
-  
-    const authQueryString = qs.parse(
-      window.location.hash.substring(1)
-    ) as IAuthQueryString;
-      
-    fetch('/api/accept-token?access_token=' + authQueryString.access_token + '&expires_in=' + authQueryString.expires_in).then(function(response) {
+    const urlParams = new URLSearchParams(window.location.hash.replace("#","?"));  
+    fetch('/api/accept-token?access_token=' + urlParams.get('access_token') + '&expires_in=' + urlParams.get('expires_in')).then(function(response) {
       return response.text().then(function(text) {
         window.location.href = text;
       });
