624069751c1d2832893a6b58b83258fd1e767dfa
Espen Strømland <espen.stromland@skatteetaten.no>
2020-01-08 10:08:50 +0100

    removed fetching tags if not found

--- src/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/containers/VersionTable/VersionTable.state.ts
+++ src/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/containers/VersionTable/VersionTable.state.ts
@@ -1,31 +1,20 @@
 import { ImageTagType } from 'models/ImageTagType';
 import { IImageTag } from 'services/auroraApiClients/imageRepositoryClient/query';
 import { ReduxProps, RootState } from 'store/types';
-import { fetchVersions } from 'store/state/versions/action.creators';
 
 export interface IVersionTableProps {
   onConfirmDeploy: (version: string) => void;
   hasAccessToDeploy: boolean;
   versionType: ImageTagType;
-  searchText?: string;
-  repository: string;
   currentVersion: IImageTag;
   versionBeingDeployed?: string;
 }
-
-export const mapDispatchToProps = {
-  fetchVersions
-};
 
 export const mapStateToProps = (
   { versions }: RootState,
   { versionType }: IVersionTableProps
 ) => ({
-  imageTagsConnection: versions.types[versionType],
-  isFetching: versions.isFetching
+  imageTagsConnection: versions.types[versionType]
 });
 
-export type VersionTableState = ReduxProps<
-  typeof mapDispatchToProps,
-  typeof mapStateToProps
->;
+export type VersionTableState = ReduxProps<{}, typeof mapStateToProps>;
