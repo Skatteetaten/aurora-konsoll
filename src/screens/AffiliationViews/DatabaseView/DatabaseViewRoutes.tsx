1962aefe331f3579f3dac55a4b0d08b6e6b87401
Martin Rødland <Martin.Rodland@skatteetaten.no>
2020-02-12 14:52:58 +0100

    fix prettier errors

--- src/screens/AffiliationViews/DatabaseView/DatabaseViewRoutes.tsx
+++ src/screens/AffiliationViews/DatabaseView/DatabaseViewRoutes.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 
-import {SchemaConnected} from './schemaConnected';
-import {Redirect, Route, Switch, useRouteMatch} from 'react-router';
-import TabLink, {TabLinkWrapper} from "../../../components/TabLink";
+import { SchemaConnected } from './schemaConnected';
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router';
+import TabLink, { TabLinkWrapper } from '../../../components/TabLink';
 
 export interface IDatabaseViewRoutesProps {
   affiliation: string;
 }
 
 export const DatabaseViewRoutes: React.FC<IDatabaseViewRoutesProps> = ({
-                                                                         affiliation
-                                                                       }) => {
+  affiliation
+}) => {
   const match = useRouteMatch();
   if (!match) {
     return null;
@@ -20,12 +20,14 @@ export const DatabaseViewRoutes: React.FC<IDatabaseViewRoutesProps> = ({
     <>
       <TabLinkWrapper>
         <TabLink to={`${match.url}/schemas`}>Aktive Skjema</TabLink>
-        <TabLink to={`${match.url}/restorableSchemas`}>Gjenopprett Skjema</TabLink>
+        <TabLink to={`${match.url}/restorableSchemas`}>
+          Gjenopprett Skjema
+        </TabLink>
       </TabLinkWrapper>
       <Switch>
-        <Redirect exact={true} from={match.url} to={`${match.url}/schemas`}/>
+        <Redirect exact={true} from={match.url} to={`${match.url}/schemas`} />
         <Route path={`${match.url}/schemas`}>
-          <SchemaConnected affiliation={affiliation}/>
+          <SchemaConnected affiliation={affiliation} />
         </Route>
         <Route path={`${match.url}/restorableSchemas`}>
           <div> Hepp</div>
