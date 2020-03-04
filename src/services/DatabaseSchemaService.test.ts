b5ab4d745ff37c7763debfc0062270ef34842470
Martin Rødland <Martin.Rodland@skatteetaten.no>
2020-03-04 13:50:55 +0100

    remove duplication value from test

--- src/services/DatabaseSchemaService.test.ts
+++ src/services/DatabaseSchemaService.test.ts
@@ -53,7 +53,6 @@ describe('DatabaseSchemaService', () => {
         createdBy: '',
         engine: 'ORACLE',
         description: null,
-        engine: 'ORACLE',
         discriminator: 'db',
         environment: 'env',
         jdbcUser: { jdbcUrl: 'test.no', password: '123', username: 'username' }
