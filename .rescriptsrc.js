25c4595d61e96cba4115e65d7d4aa35fda58b622
Anders Mikkelsen <Anders.Mikkelsen@skatteetaten.no>
2020-10-27 14:15:40 +0100

    div fix

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = config => {
   if (process.env.NODE_ENV === 'production') {
-    config.output.libraryTarget = 'umd';
+    config.output.libraryTarget = 'system';
   }
   config.entry = './src/skatteetaten-aurora-konsoll.tsx';
   config.externals = ['react', 'react-dom', '@skatteetaten/frontend-components'];
