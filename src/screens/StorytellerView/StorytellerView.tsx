0b56de7bc7618ecd198619be9c79892b4d5d4f11
Anders Mikkelsen <Anders.Mikkelsen@skatteetaten.no>
2021-05-18 11:10:56 +0200

    test storytell wrapper with overflow auto

--- src/screens/StorytellerView/StorytellerView.tsx
+++ src/screens/StorytellerView/StorytellerView.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export const StorytellerView = () => {
-  return <div id="storyteller-attach"></div>;
+  return <StorytellerWrapper id="storyteller-attach"></StorytellerWrapper>;
 };
+
+const StorytellerWrapper = styled.div`
+  flex: 1;
+  position: relative;
+  overflow: auto;
+`;
