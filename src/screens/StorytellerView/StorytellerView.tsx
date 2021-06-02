e112173f14dc789c5ada6f8bfcbdf42347f99b38
Engebretsen, Tomas <Tomas.Engebretsen@skatteetaten.no>
2021-06-02 13:18:13 +0200

    Set fixed height

--- src/screens/StorytellerView/StorytellerView.tsx
+++ src/screens/StorytellerView/StorytellerView.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const StorytellerView = () => {
-  return <StorytellerWrapper id="storyteller-attach"></StorytellerWrapper>;
+  return <StorytellerWrapper id="storyteller-attach"/>;
 };
 
 const StorytellerWrapper = styled.div`
   flex: 1;
   position: relative;
   overflow: auto;
+  height: 100%;
 `;
