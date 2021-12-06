ffe55652dcc9ae31a1da27c398e0340219166cec
Martin Rodland <Martin.Rodland@skatteetaten.no>
2021-12-06 14:17:12 +0100

    removed unused

--- src/web/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/containers/RedeployRowAndVersionTable/RedeployRowAndVersionTable.state.ts
+++ src/web/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/containers/RedeployRowAndVersionTable/RedeployRowAndVersionTable.state.ts
@@ -1,13 +1,9 @@
 import { ImageTagType } from 'web/models/ImageTagType';
 import { VersionStatus } from '../../../models/VersionStatus';
 import { IImageTag } from 'web/services/auroraApiClients/imageRepositoryClient/query';
-import {
-  deploy,
-  refreshApplicationDeployment,
-} from 'web/store/state/applicationDeployments/action.creators';
+import { deploy } from 'web/store/state/applicationDeployments/action.creators';
 import { ReduxProps, RootState } from 'web/store/types';
 import { AuroraConfigFileResource } from 'web/services/auroraApiClients/applicationDeploymentClient/query';
-import { addCurrentErrors } from 'web/screens/ErrorHandler/state/actions';
 
 export interface IRedeployRowAndVersionTableProps {
   versionType: ImageTagType;
@@ -22,15 +18,12 @@ export interface IRedeployRowAndVersionTableProps {
 
 export const mapDispatchToProps = {
   deploy,
-  addCurrentErrors,
-  refreshApplicationDeployment,
 };
 
 export const mapStateToProps = ({ applications, versions }: RootState) => {
-  const { isDeploying, isRefreshing } = applications;
+  const { isDeploying } = applications;
   const { configuredVersionTag, isFetchingConfiguredVersionTag } = versions;
   return {
-    isRefreshing,
     isDeploying,
     configuredVersionTag,
     isFetchingConfiguredVersionTag,
