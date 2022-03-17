9fbb57a6cb3a885d3d2d0b4dde4580a5b6513c50
Engebretsen, Tomas <Tomas.Engebretsen@skatteetaten.no>
2022-03-17 16:50:56 +0100

    Utbedrer søk

--- src/web/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/containers/VersionTable/VersionTable.tsx
+++ src/web/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/containers/VersionTable/VersionTable.tsx
@@ -86,7 +86,9 @@ export const VersionTable = ({
     .filter((it) => it.name !== versionToFilter)
     .map((it) => ({
       type: getOptionName(it.type),
-      name: it.name,
+      name: versionType == ImageTagType.SEARCH
+          ? <HighlightedValues searchText={searchText} text={it.name}/>
+          : it.name,
       lastModified: it.image ? (
         <DateWithTooltip date={it.image.buildTime} position="left" />
       ) : (
@@ -211,4 +213,66 @@ const SearchInfo = styled.div`
   background-color: ${skeColor.lightBlue};
   color: ${skeColor.blue100};
   padding: 12px;
+`;
+
+const HighlightedValues = (props: {searchText: string, text: string}) => {
+
+  const {searchText, text} = props;
+  const lcText = text.toLowerCase();
+
+  interface IIndices {
+    start: number,
+    end: number
+  }
+
+  // Finner indekser for treff i søk:
+  const indicesList: IIndices[] = searchText
+      .split(' ')
+      .map((value) => value.trim().toLowerCase())
+      .filter(v => v.length > 0)
+      .flatMap(v => {
+        let startIndex = 0, index = 0, indices: IIndices[] = [];
+        while ((index = lcText.indexOf(v, startIndex)) > -1) {
+          indices.push({start: index, end: index + v.length});
+          startIndex = index + 1;
+        }
+        return indices;
+      })
+      .sort((a, b) =>  a!.start - b!.start);
+
+  if (indicesList.length == 0) return null;
+
+  // Fletter sammen overlappende treff:
+  let mergedList: IIndices[] = [], current: IIndices = indicesList[0];
+  for (let i = 0; i < indicesList.length; i++) {
+    if (i == indicesList.length - 1) {
+      mergedList.push(current);
+    } else {
+      const next = indicesList[i+1];
+      if (next.start < current.end) {
+        if (next.end > current.end) current.end = next.end;
+      } else {
+        mergedList.push(current);
+        current = next;
+      }
+    }
+  }
+
+  // Formaterer resultat:
+  const startOfString = mergedList[0].start > 0 ? [<>{text.slice(0, mergedList[0].start)}</>] : [];
+  return <>
+    {startOfString.concat(
+        mergedList.flatMap((indices, i) => {
+          const matchingString = <Highlight key={`h${i}`}>{text.slice(indices.start, indices.end)}</Highlight>;
+          const afterMatchingString = text.slice(indices.end, mergedList[i + 1]?.start);
+          return afterMatchingString.length == 0
+              ? [matchingString]
+              : [matchingString, <>{afterMatchingString}</>];
+        })
+    )}
+  </>;
+}
+
+const Highlight = styled.span`
+  background-color: ${skeColor.lightBrown};
 `;
