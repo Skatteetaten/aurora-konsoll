8b5c9cb0cecd7fa6321662acf803a813ff200706
Martin Rodland <Martin.Rodland@skatteetaten.no>
2022-04-08 10:38:01 +0200

    refactor: display branch info in callout not messageBar

--- src/web/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/components/MissingGitBranchInformation.tsx
+++ src/web/screens/AffiliationViews/DeploymentView/DetailsView/VersionView/components/MissingGitBranchInformation.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { MessageBar } from '@skatteetaten/frontend-components/MessageBar';
+import Callout from '@skatteetaten/frontend-components/Callout';
+import CalloutButton from 'web/components/CalloutButton';
 
-export const InvalidRefInformation: React.FC<{
-  refName: string | undefined;
-}> = ({ refName }) => (
-  <MessageBar type={MessageBar.Type.warning}>
-    Ikke mulig å deploye nåværende eller annen versjon. Årsak: Finner ikke
-    branchen <b>{refName}</b> applikasjonen ble deployet med. Løsning: Dersom
-    branchen har blitt slettet: deploy applikasjonen ved hjelp av
-    kommandolinjeverktøyet AO med en branch som eksisterer. Applikasjonen vil da
-    kunne deployes i Aurora Konsoll.
-  </MessageBar>
-);
+export const BranchInformation: React.FC<{
+  gitReference: string | undefined;
+  isBranchDeleted: boolean;
+}> = ({ gitReference, isBranchDeleted }) => {
+  return (
+    <CalloutButton
+      calloutProps={{
+        color: isBranchDeleted ? Callout.ERROR : Callout.INFO,
+        gapSpace: 8,
+        beakWidth: 2,
+        calloutWidth: '400px',
+        directionalHint: Callout.POS_BOTTOM_CENTER,
+      }}
+      buttonProps={{
+        icon: isBranchDeleted ? 'warning' : 'info',
+        color: isBranchDeleted ? 'red' : 'blue',
+      }}
+      title={`Deployet med branch ${gitReference}`}
+      content={
+        <>
+          {isBranchDeleted ? (
+            <p>
+              Applikasjonen har blitt deployet med en Aurora Config branch som
+              er slettet. Løsning: Deploy applikasjonen med en annen branch som
+              eksisterer, eller gjennopprett slettet branch.
+            </p>
+          ) : (
+            <p>
+              Applikasjonen vil deployes med Aurora Config branchen den ble
+              deployet med så lenge det ikke endres manuelt i dialogen man får
+              opp når man trykker på deploy.
+            </p>
+          )}
+        </>
+      }
+    />
+  );
+};
