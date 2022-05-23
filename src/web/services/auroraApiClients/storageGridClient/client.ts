1ef3a2c04908bb93a87b090f8b3006670b8c343b
Martin Rodland <Martin.Rodland@skatteetaten.no>
2022-05-23 12:46:08 +0200

    feat: made storageGridClient

---
+++ src/web/services/auroraApiClients/storageGridClient/client.ts
@@ -0,0 +1,73 @@
+import GoboClient, { IDataAndErrors } from 'web/services/GoboClient';
+import {
+  ObjectAreas,
+  StorageGridObjectArea,
+  StorageGridQuery,
+  STORAGEGRID_AREAS_QUERY,
+  STORAGEGRID_TENANT_QUERY,
+  Tenant,
+} from './query';
+
+export interface AreasAndTenant {
+  activeAreas?: StorageGridObjectArea[];
+  isTenantRegistered?: boolean;
+}
+
+export class StorageGridClient {
+  private client: GoboClient;
+
+  constructor(client: GoboClient) {
+    this.client = client;
+  }
+
+  public async getAreasAndTenant(
+    affiliation: string
+  ): Promise<IDataAndErrors<AreasAndTenant>> {
+    const tenant = await this.getTenant(affiliation);
+
+    const isTenantRegistered =
+      tenant.data?.affiliations.edges[0].node.storagegrid.tenant.isRegistered;
+
+    if (isTenantRegistered === true) {
+      const areas = await this.getAreas(affiliation);
+      const activeAreas =
+        areas.data?.affiliations.edges[0].node.storagegrid.objectAreas.active;
+
+      return {
+        name: areas.name,
+        data: {
+          isTenantRegistered,
+          activeAreas: activeAreas,
+        },
+        errors: areas.errors,
+      };
+    }
+
+    return {
+      name: tenant.name,
+      data: {
+        isTenantRegistered,
+        activeAreas: undefined,
+      },
+      errors: tenant.errors,
+    };
+  }
+
+  public async getAreas(
+    affiliation: string
+  ): Promise<IDataAndErrors<StorageGridQuery<ObjectAreas>>> {
+    return await this.client.query<StorageGridQuery<ObjectAreas>>({
+      query: STORAGEGRID_AREAS_QUERY,
+      variables: { affiliation },
+    });
+  }
+
+  public async getTenant(
+    affiliation: string
+  ): Promise<IDataAndErrors<StorageGridQuery<Tenant>>> {
+    return await this.client.query<StorageGridQuery<Tenant>>({
+      query: STORAGEGRID_TENANT_QUERY,
+      variables: { affiliation },
+    });
+  }
+}
---
+++ src/web/services/auroraApiClients/storageGridClient/query.ts
@@ -0,0 +1,80 @@
+import gql from 'graphql-tag';
+
+interface StorageGridObjectAreaStatus {
+  message: string;
+  reason: string;
+  success: boolean;
+}
+
+export interface StorageGridObjectArea {
+  bucketName: string;
+  creationTimestamp: string;
+  name: string;
+  namespace: string;
+  status: StorageGridObjectAreaStatus;
+  tenantName: string;
+}
+
+export interface ObjectAreas {
+  objectAreas: {
+    active: StorageGridObjectArea[];
+  };
+}
+
+export interface Tenant {
+  tenant: {
+    isRegistered: boolean;
+  };
+}
+
+export interface StorageGridQuery<T extends Tenant | ObjectAreas> {
+  affiliations: {
+    edges: {
+      node: {
+        storagegrid: T;
+      };
+    }[];
+  };
+}
+
+export const STORAGEGRID_TENANT_QUERY = gql`
+  query tenant($affiliation: String!) {
+    affiliations(names: [$affiliation]) {
+      edges {
+        node {
+          storagegrid {
+            tenant {
+              isRegistered
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const STORAGEGRID_AREAS_QUERY = gql`
+  query areas($affiliation: String!) {
+    affiliations(names: [$affiliation]) {
+      edges {
+        node {
+          storagegrid {
+            objectAreas {
+              active {
+                name
+                namespace
+                bucketName
+                creationTimestamp
+                status {
+                  message
+                  reason
+                  success
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
