fab21656e6c416902596d6ec48dc5e3f2de1a0d5
Martin Rodland <Martin.Rodland@skatteetaten.no>
2022-05-24 08:43:40 +0200

    feat: added reducer and actions for StorageGrid

---
+++ src/web/store/state/storageGrid/action.creators.ts
@@ -0,0 +1,14 @@
+import { doAsyncActions } from 'web/utils/redux/action-utils';
+import { actions } from './actions';
+
+export function getAreasAndTenant(affiliation: string) {
+  return doAsyncActions(actions.fetchAreasAndTenant, (clients) => {
+    return clients.storageGridClient.getAreasAndTenant(affiliation);
+  });
+}
+
+export function getAreas(affiliation: string) {
+  return doAsyncActions(actions.fetchAreas, (clients) => {
+    return clients.storageGridClient.getAreas(affiliation);
+  });
+}
---
+++ src/web/store/state/storageGrid/reducer.ts
@@ -0,0 +1,53 @@
+import { actions } from './actions';
+import { createReducer } from '@reduxjs/toolkit';
+import { AreasAndTenant } from 'web/services/auroraApiClients';
+
+interface StorageGridState {
+  isFetchingAreas: boolean;
+  isFetchingAreasAndTenant: boolean;
+  areasAndTenant?: AreasAndTenant;
+  errors: Error[];
+}
+
+const initialState: StorageGridState = {
+  isFetchingAreas: false,
+  isFetchingAreasAndTenant: false,
+  errors: [],
+};
+
+export const storageGridReducer = createReducer(initialState, (builder) => {
+  builder.addCase(actions.fetchAreasAndTenant.request, (state) => {
+    state.isFetchingAreasAndTenant = true;
+  });
+  builder.addCase(actions.fetchAreasAndTenant.success, (state, { payload }) => {
+    state.isFetchingAreasAndTenant = false;
+    if (payload.data) {
+      state.areasAndTenant = payload.data;
+    }
+    if (payload.errors) {
+      state.errors.push(...payload.errors);
+    }
+  });
+  builder.addCase(actions.fetchAreasAndTenant.failure, (state, { payload }) => {
+    state.isFetchingAreasAndTenant = false;
+    state.errors.push(payload);
+  });
+
+  builder.addCase(actions.fetchAreas.request, (state) => {
+    state.isFetchingAreas = true;
+  });
+  builder.addCase(actions.fetchAreas.success, (state, { payload }) => {
+    state.isFetchingAreas = false;
+    if (payload.data && state.areasAndTenant) {
+      state.areasAndTenant.activeAreas =
+        payload.data.affiliations.edges[0].node.storagegrid.objectAreas.active;
+    }
+    if (payload.errors) {
+      state.errors.push(...payload.errors);
+    }
+  });
+  builder.addCase(actions.fetchAreas.failure, (state, { payload }) => {
+    state.isFetchingAreas = false;
+    state.errors.push(payload);
+  });
+});
